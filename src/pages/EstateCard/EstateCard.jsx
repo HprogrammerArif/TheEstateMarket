d08de0b4de17ae1fb682308f37db81dad41758ca
HprogrammerArif <programmerhabluarif@gmail.com>
2024-04-17 10:47:09 +0600

    added npm animatedcss packege

--- src/pages/EstateCard/EstateCard.jsx
+++ src/pages/EstateCard/EstateCard.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import 'animate.css';
 
 const EstateCard = ({estate}) => {
 
@@ -11,16 +12,16 @@ const EstateCard = ({estate}) => {
     <div>
       <div className="card card-compact  bg-base-100 shadow-xl ">
         <figure>
-          <img className="max-h-[270px] w-full"
+          <img className="max-h-[270px] w-full animate__animated animate__pulse"
             src={image}
           />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{estate_title}</h2>
+          <h2 className="card-title animate__animated animate__bounce animate__delay-1s">{estate_title}</h2>
           <p>{description}</p>
           <div className="card-actions">
             <Link to={`/details/${id}`}>
-            <button className="btn bg-violet-500">View Details</button>
+            <button className="btn bg-violet-500 animate__animated animate__pulse animate__delay-2s">View Details</button>
             </Link>
           </div>
         </div>
